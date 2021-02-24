94c1ef6138f5bf156abfef6b7dd01588cfe8ad2d
Александр Тетенков <tetenkov@tecon.ru>
2021-02-24 12:51:34 +0300

    Добавил сайт для сдачи в GB

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    links = []
-    links.append(Markup('<iframe width="560" height="315" src="https://www.youtube.com/embed/5qap5aO4i9A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'))
-    links.append(Markup('<iframe src="https://player.vimeo.com/video/39880101" width="560" height="315" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>'))
-    return render_template('index.html', links_list=links)
+    # links = []
+    # links.append(Markup('<iframe width="560" height="315" src="https://www.youtube.com/embed/5qap5aO4i9A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'))
+    # links.append(Markup('<iframe src="https://player.vimeo.com/video/39880101" width="560" height="315" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>'))
+    return render_template('index2.html')
+
 
 
 if __name__ == '__main__':
